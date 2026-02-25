43c9404add2370ca37aefaa3a3eb0a334c593bc6
m.fortunato <m.fortunato@360consulenza.com>
2026-02-25 17:24:33 +0100

    arrived at lesson 65 - using local storage to 'save' tasks

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -3,6 +3,14 @@ import {Injectable} from '@angular/core';
 
 @Injectable({ providedIn: 'root' })
 export class TasksService {
+
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   private tasks =  [
     {
       id: 't1',
@@ -40,9 +48,15 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id );
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
