87e8e0748aef73b69b6095c1e3fbf4691b6953e1
m.fortunato <m.fortunato@360consulenza.com>
2026-02-26 11:02:15 +0100

    arrived at lesson 69 - new branch for exploring ngmodule classes

---
+++ src/app/app.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+
+// takes a configuration object to configure the module
+@NgModule({
+  //declare and register all the components
+  declarations: []
+})
+export class AppModule {
+
+}
